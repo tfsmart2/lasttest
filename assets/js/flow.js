515872d8e0eed40024db41c6d834ae9113a64374
tfsmart2 <74022167+tfsmart2@users.noreply.github.com>
2021-04-07 18:33:15 +0200

    Update flow.js

--- assets/js/flow.js
+++ assets/js/flow.js
@@ -470,7 +470,10 @@ const accountData = async () => {
   $('#alldeposited').text(totaldeposited.toFixed(2));
   const userpayout = (invester.payoutSum.toNumber() / 1000000);
   $('#userpayout').text(userpayout.toFixed(2)); 
-  const loss = totaldeposited - userpayout;	  
+  const loss = totaldeposited - userpayout;
+	  if (parseInt(loss) < 0 ){
+	  loss = 0;
+	  }
 	  
 /* if ( parseFloat($('#alldeposited').text()) >  parseFloat($('#userpayout').text()) ){
 
